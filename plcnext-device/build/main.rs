a2d57e6606503141fe94052604106b6c50c9ccd6
Martin Boers <mboers@phoenixcontact.com>
2019-11-14 10:19:58 +0100

    Work in progress

---
+++ plcnext-device/build/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+fn main() {
+    let mut my_config = cpp_build::Config::new();
+    // TODO: Replace the path with an ENV var.
+    // Check this for guidance:
+    // https://github.com/woboq/qmetaobject-rs/blob/master/qmetaobject/build.rs
+    my_config.include("/opt/pxc/sdk/AXCF2152/2019.6/sysroots/cortexa9t2hf-neon-pxc-linux-gnueabi/usr/include/plcnext");
+    my_config.build("src/lib.rs");
+
+    // Tell cargo to tell rustc to link the correct libraries.
+    println!("cargo:rustc-link-lib=Arp.Device.Interface");
+    println!("cargo:rustc-link-lib=Arp.Io.Axioline");
+}
